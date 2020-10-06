48c10ee322fa9d2726c1fed0a15e264b536278bc
RaphZap <36986166+RaphZap@users.noreply.github.com>
2020-10-06 09:04:52 +0800

    removed tictactoe. added todo app

---
+++ src/AddTodo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class AddTodo extends React.Component {
+    state = {
+        content: ' '
+    }
+
+    handleSubmit = (ev) => {
+        ev.preventDefault();
+        this.props.addTodo(this.state);
+        this.setState({
+            content: ''
+        });
+    }
+
+    handleChange = (ev) => {
+        this.setState({
+            content: ev.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Add new todo: </label>
+                    <input type="text" onChange={this.handleChange} value={this.state.content}/>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AddTodo;
