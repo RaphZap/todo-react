6b135fcd886770565af09e0817be3a2e393b815a
RaphZap <36986166+RaphZap@users.noreply.github.com>
2020-10-21 14:45:21 +0800

    used uuid for ids

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Todos from './Todos';
 import AddTodo from './AddTodo';
+import { v4 as uuid4 } from 'uuid';
 
 class App extends React.Component {
   state = {
      todos : [
        {id: 1, content: 'mark items as finished', isDone: true},
        {id: 2, content: 'add a database', isDone: false},
-       {id: 3, content: 'confirm deletion of item', isDone: false}
+       {id: 3, content: 'confirm deletion of item', isDone: true},
+       {id: 4, content: 'maybe use modals instead of alert()/confirm()', isDone: false}
      ]
   }
 
@@ -25,13 +27,20 @@ class App extends React.Component {
   }
 
   addTodo = (todo) => {
-    // todo.id = Math.random();
-    todo.id = this.state.todos.length ? this.state.todos.length + 1 : 1;
+    // alert if item is blank
+    if (todo.content.length === 0 || todo.content.trim() === '') {
+      alert('Todo item is empty!');
+      return;
+    }
+
+    // npm install uuid
+    todo.id = uuid4();
+    // todo.id = this.state.todos.length ? this.state.todos.length + 1 : 1;
     let todos = [...this.state.todos, todo];
     this.setState({
       todos
     });
-    // console.log(this.state);
+    console.log(this.state);
   }
 
   markTodo = (id) => {  
